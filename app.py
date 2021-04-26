5eeee3611e7b297b6f963538c709ed1e8a1bb4e5
James Lee <jlee22@babson.edu>
2021-04-25 22:54:45 -0400

    idk whats going on

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, flash, render_template, request, redirect, url_for
 from backend import write
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
 ALLOWED_EXTENSIONS = {'xlsx'}
+def allowed_file(filename):
+   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 app = Flask(__name__)
@@ -31,18 +33,34 @@ def courselisting():
 def howto():
     return render_template("howto.html")
 
+
 @app.route("/registration.html", methods = ["GET", "POST"])
-@app.route('/upload')
-def upload_file():
+def registration():
+   if request.method == 'POST':
+       if 'file' not in request.files:
+           print('No file attached in request')
+           return redirect(request.url)
+       file = request.files['file']
+       if file.filename == '':
+           print('No file selected')
+           return redirect(request.url)
+       if file and allowed_file(file.filename):
+           filename = secure_filename(file.filename)
+           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           write(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           return render_template('output.html')
    return render_template('registration.html')
 
-@app.route('/output.html', methods = ['GET', 'POST'])
-def uploaded_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      write(f.filename)
-      return render_template('output.html')
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#    return render_template('output.html')
+
+# @app.route('/output.html', methods = ['GET', 'POST'])
+# def uploaded_file():
+#     if request.method == 'POST':
+#         f = request.files['file']
+#         f.save(secure_filename(f.filename))
+#         return write(f)
     
 
 if __name__ == '__main__':
